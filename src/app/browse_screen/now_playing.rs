74424b32e71176c0df283ad828fd0ff684cde518
LeoRiether <leonardoalves8217@gmail.com>
2023-03-25 11:03:13 -0300

    Render volume bar in now_playing

--- src/app/browse_screen/now_playing.rs
+++ src/app/browse_screen/now_playing.rs
@@ -16,6 +16,7 @@ pub struct NowPlaying {
     pub time_pos: i64,
     pub time_rem: i64,
     pub paused: bool,
+    pub volume: i64,
 }
 
 impl NowPlaying {
@@ -25,6 +26,7 @@ impl NowPlaying {
         self.time_pos = mpv.get_property("time-pos").unwrap_or_default();
         self.time_rem = mpv.get_property("time-remaining").unwrap_or_default();
         self.paused = mpv.get_property("pause").unwrap_or_default();
+        self.volume = mpv.get_property("volume").unwrap_or_default();
     }
 
     pub fn render(&self, frame: &mut Frame<'_, MyBackend>, chunk: Rect) {
@@ -40,7 +42,8 @@ impl NowPlaying {
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .constraints([
-                Constraint::Percentage(30),
+                Constraint::Percentage(20),
+                Constraint::Length(1),
                 Constraint::Length(strlen(&playback_left_str) as u16),
                 Constraint::Min(5),
                 Constraint::Length(strlen(&playback_right_str) as u16),
@@ -65,8 +68,20 @@ impl NowPlaying {
             Paragraph::new(Spans::from(parts)).alignment(Alignment::Center)
         };
 
+        let volume_paragraph = {
+            let left = "─"
+                .repeat((self.volume as usize * chunks[0].width as usize / 100).saturating_sub(1));
+            let indicator = "■";
+            let right = "─".repeat((chunks[0].width as usize).saturating_sub(left.len()));
+            Paragraph::new(Spans::from(vec![
+                Span::styled(left, Style::default().fg(Color::White)),
+                Span::styled(indicator, Style::default().fg(Color::White)),
+                Span::styled(right, Style::default().fg(Color::DarkGray)),
+            ]))
+        };
+
         let playback_bar_str: String = {
-            let mut s: Vec<_> = "─".repeat(chunks[2].width as usize).chars().collect();
+            let mut s: Vec<_> = "─".repeat(chunks[3].width as usize).chars().collect();
             let i = self.percentage as usize * s.len() / 100;
             s[i] = '■';
             s.into_iter().collect()
@@ -80,8 +95,9 @@ impl NowPlaying {
             Paragraph::new(playback_right_str).style(Style::default().fg(Color::White));
 
         frame.render_widget(media_title, lines[0]);
-        frame.render_widget(playback_left, chunks[1]);
-        frame.render_widget(playback_bar, chunks[2]);
-        frame.render_widget(playback_right, chunks[3]);
+        frame.render_widget(volume_paragraph, chunks[0]);
+        frame.render_widget(playback_left, chunks[2]);
+        frame.render_widget(playback_bar, chunks[3]);
+        frame.render_widget(playback_right, chunks[4]);
     }
 }
