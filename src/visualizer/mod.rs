086ed20f680b81314ff374c4c5df1ee1ddb24b12
LeoRiether <leonardoalves8217@gmail.com>
2023-05-16 17:45:03 -0300

    Panic if we can't read the full `cava` chunk

--- src/visualizer/mod.rs
+++ src/visualizer/mod.rs
@@ -69,21 +69,6 @@ impl Visualizer {
                 format!("Failed to spawn the visualizer process. Is `cava` installed? The received error was: {}", e)
             })?;
 
-        // Read first chunk to make sure we can read `buf.len` bytes
-        let mut buf = vec![0_u8; 2 * opts.bars];
-        {
-            let stdout = process.stdout.as_mut().unwrap();
-            match stdout.read_exact(&mut buf) {
-                Err(e) if e.kind() == io::ErrorKind::UnexpectedEof => {
-                    let buf_str = String::from_utf8_lossy(&buf);
-                    return Err(
-                        format!("Failed to spawn the visualizer process correctly. The first received chunk was: {}", buf_str).into()
-                    );
-                }
-                _ => {}
-            }
-        }
-
         let data = Arc::new(Mutex::new(vec![0_u16; opts.bars]));
         let stop_flag = Arc::new(AtomicBool::new(false));
 
@@ -91,9 +76,15 @@ impl Visualizer {
             let data = data.clone();
             let stop_flag = stop_flag.clone();
             thread::spawn(move || {
+                let mut buf = vec![0_u8; 2 * opts.bars];
                 while !stop_flag.load(atomic::Ordering::Relaxed) {
                     let stdout = process.stdout.as_mut().unwrap();
-                    stdout.read_exact(&mut buf).unwrap();
+                    let read_res = stdout.read_exact(&mut buf);
+
+                    if let Err(e) = read_res {
+                        let buf_str = String::from_utf8_lossy(&buf);
+                        panic!("Failed to read from the visualizer process. The received error was: {}. Process output: {}", e, buf_str);
+                    }
 
                     let mut data = data.lock().unwrap();
                     for i in 0..data.len() {
