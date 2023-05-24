85cc73cdf70ce97a00cf9bed8ea624686fde8cf0
LeoRiether <leonardoalves8217@gmail.com>
2023-05-24 16:26:33 -0300

    Debugging CI: remove icon for Windows executables

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("assets/tori.ico");
-        res.compile()?;
-    }
+    // if cfg!(target_os = "windows") {
+    //     let mut res = winres::WindowsResource::new();
+    //     res.set_icon("assets\\tori.ico");
+    //     res.compile()?;
+    // }
 
     Ok(())
 }
