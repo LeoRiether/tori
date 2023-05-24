cb5bd4a5046dd27a3d19a203d4576dd7e3f27258
LeoRiether <leonardoalves8217@gmail.com>
2023-05-24 14:21:00 -0300

    Debugging CI: change icon path

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("../assets/tori.ico");
+        res.set_icon("assets/tori.ico");
         res.compile()?;
     }
 
